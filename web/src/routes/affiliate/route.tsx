a1e5fcba64194343b3f7677c61cff8a64723c9fe
haerincode <oskarpardo22@gmail.com>
2026-09-13 16:36:11 -0300

    remove: delete affiliate auth route

--- web/src/routes/affiliate/route.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { AffiliateAuthPage } from '@/features/affiliates'
-
-export const Route = createFileRoute('/affiliate')({
-  component: AffiliateAuthPage,
-})
