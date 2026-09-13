1425c751300916d6faea09d7bc9a89cb4d3ab2fc
haerincode <oskarpardo22@gmail.com>
2026-09-13 17:42:10 -0300

    fix: integrate affiliates with main user authentication system

--- router/affiliate.go
+++ router/affiliate.go
@@ -15,7 +15,7 @@ func SetAffiliateRouter(router *gin.Engine) {
 		affiliateRoute.POST("/register", controller.RegisterAffiliate)
 		affiliateRoute.POST("/login", controller.AffiliateLogin)
 
-		// Protected endpoints (require user authentication)
+		// Protected endpoints - require main user authentication
 		affiliateRoute.Use(middleware.UserAuth())
 		affiliateRoute.Use(middleware.AffiliateAuth())
 		affiliateRoute.GET("/dashboard", controller.GetAffiliateDashboard)
