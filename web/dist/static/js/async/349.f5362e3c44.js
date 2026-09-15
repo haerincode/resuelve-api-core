95b82f9275fa3170ff8205a7820a390632c8a898
haerincode <oskarpardo22@gmail.com>
2026-09-15 12:13:13 -0300

    Add web/dist for Heroku deployment

---
+++ web/dist/static/js/async/349.f5362e3c44.js
@@ -0,0 +1,2 @@
+"use strict";(self.rspackChunknewapi_web=self.rspackChunknewapi_web||[]).push([[349],{89532(e,s,n){n.r(s);let p=n(47745).GN;n.d(s,{},{component:p})}}]);
+//# sourceMappingURL=349.f5362e3c44.js.map
---
+++ web/dist/static/js/async/8458.8b1f1d9425.js
@@ -0,0 +1,2 @@
+"use strict";(self.rspackChunknewapi_web=self.rspackChunknewapi_web||[]).push([[8458],{8209(e,s,n){n.r(s);let p=n(2527).m;n.d(s,{},{component:p})}}]);
+//# sourceMappingURL=8458.8b1f1d9425.js.map
